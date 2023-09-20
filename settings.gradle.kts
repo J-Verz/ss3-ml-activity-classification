6ac2f4b8bd8f9e7bd01387c501e6ff55b6e76bfc
Johan Verzijden <johan@verzijden.net>
2023-09-20 10:41:57 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SS2 ML Activity Detection Test"
+include(":app")
